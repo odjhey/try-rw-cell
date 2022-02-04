208ff4f45951b09e787a6edfd3c3265319bc0173
odjhey <odjhey@gmail.com>
2022-02-04 15:47:58 +0800

    refactor: customFetch and add gql variables support

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import { useEffect, useState } from "react";
 
+const customFetch = async (query: string, variables: Record<string, any>) => {
+  const result = await fetch("http://localhost:3003/graphql", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      // query: "query { samuraiList { id name } }",
+      query,
+      variables,
+    }),
+  });
+
+  return result.json();
+};
+
 function App() {
   const [data, setData] = useState();
 
   useEffect(() => {
-    fetch("http://localhost:3003/graphql", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        query: "query { samuraiList { id name } }",
-      }),
-    })
-      .then((d) => {
-        return d.json();
-      })
-      .then((d) => {
-        setData(d);
-        console.log(d);
-      });
+    customFetch(
+      `
+    query Sam($limit:Int!){
+  samuraiList (pageParams:{limit: $limit, offset: 2}) {
+    id name nickname age
+  }
+}
+    `,
+      { limit: 1 }
+    ).then((result) => {
+      setData(result);
+    });
   }, []);
 
   return (
